37eda17a2961e44f5705cf1b5a5cd6ef859f5a7b
Akash Manhas <122945144+AkashRajput90@users.noreply.github.com>
2024-07-22 15:54:16 -0400

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "client_id" {
+  description = "Client ID for the Service Principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Client Secret for the Service Principal"
+  type        = string
+}
